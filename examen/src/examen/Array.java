28c40e4c147372185a1cd67c99dee0a1bac56246
al036780 <al036780@pc2209>
2019-03-15 15:57:37 +0100

    examen de entornos de desarrollo

---
+++ examen/src/examen/Array.java
@@ -0,0 +1,44 @@
+package examen;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author Cristian
+ * @version 1.0
+ */
+public class Array {
+
+	private int Array [] = {5,1,22,9,12,6,4,8,15,21,18,17,2,16};
+
+	public Array() {
+	
+	}
+
+	/**
+	 * método que sobrescribe el ToString de la clase Object
+	 */
+	@Override
+	public String toString() {
+		return "Array [Array=" + Arrays.toString(Array) + "]";
+	}
+	
+	/**
+	 * 
+	 * @param x int
+	 * @return devuelve true si lo encontramo y false si no lo encontramos
+	 */
+	public boolean busquedalinal (int x) {
+		boolean encontrado = false;
+		
+		for (int i = 0 ; i < this.Array.length ; i++) {
+			if (this.Array[i] ==  x) {
+				encontrado =true;
+			}
+		}
+		return encontrado;
+	}
+	
+	
+	
+}
